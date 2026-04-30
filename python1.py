5587cd5e78028435ee4f5c5812e27357cc2d1a03
sumith kumar <s34726158@gmail.com>
2026-04-30 08:27:54 +0530

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,6 @@
+a=10;#IntegerDatatype
+b=11.5;#Float Datatype
+c=2.05j;#ComplexNumber
+print("a is Type of",type(a));#prints type of variable a
+print("b is Type of",type(b));#prints type of variable b
+print("c is type of",type(c));#prints type of variable c
