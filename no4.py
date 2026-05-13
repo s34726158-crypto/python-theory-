fe4f0b2f92d85734ae01e07dad10807d845b50f3
sumith kumar <s34726158@gmail.com>
2026-05-13 11:17:20 +0530

    Add files via upload

---
+++ no4.py
@@ -0,0 +1,4 @@
+#Taking input from user and greeting
+name = input ("Enter your name:")
+print("Hello,",name+"!Welcome to python.")
+
---
+++ no5.py
@@ -0,0 +1,4 @@
+#basic arithmatic calculator
+a = float()(input("Enter first number:"))
+b=float(input("Enter second number:"))
+op=input("")
